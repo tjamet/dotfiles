49e20b9b9149f8b6f6c567519458917fb5fcba32
Thibault Jamet <tjamet@users.noreply.github.com>
2024-11-14 01:50:43 +0100

    Ensure xcode tools is installed on mac

--- .install-brew.sh
+++ .install-brew.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 #
+
+case $(uname -s) in
+Darwin)
+xcode-select --install
+;;
+esac
+
 type brew 2>/dev/null >/dev/null && exit
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 
